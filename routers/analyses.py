4d191436882842799d28b9de743cf5af9fbff35d
sobertrauter <robert_sauter@gmx.de>
2024-01-22 16:09:41 +0100

    add statistical values to analysis

--- routers/analyses.py
+++ routers/analyses.py
@@ -9,6 +9,8 @@ from lib.globals import OAUTH2_SCHEME
 from models.Filter import Filter
 from lib.http_exceptions import HTTP_422
 from models.Hackathon import Hackathon
+import statistics
+from models.Analysis import SingleAnalysisMeasure, CategoryAnalysisMeasure, Analysis
 
 router = APIRouter()
 
@@ -79,6 +81,50 @@ def build_single_hackathon(hackathon_ids: list[str], hackathons_collection: Coll
             combine_hackathon_values_strict(final_hackathon['results'], hackathon['results'])
     return final_hackathon
 
+def create_statistics(values: list[str | int]) -> SingleAnalysisMeasure | CategoryAnalysisMeasure:
+    ''
+    participants = len(values)
+    distribution = {}
+    for value in values:
+        value_as_string = str(value)
+        if value_as_string in distribution:
+            distribution[value_as_string] += 1
+        else:
+            distribution[value_as_string] = 1
+    if type(values[0]) is int:
+        deviation = statistics.stdev(values) if len(values) > 1 else 0
+        return SingleAnalysisMeasure(
+            participants=participants,
+            average=statistics.fmean(values),
+            deviation=deviation,
+            distribution=distribution
+        )
+    else:
+        return CategoryAnalysisMeasure(
+            participants=participants,
+            distribution=distribution
+        )
+
+def create_analysis(hackathon: dict) -> Analysis:
+    '''Create an object, that contains statistical values for every measure of a survey'''
+    Hackathon.model_validate(hackathon)
+    statistics = {}
+    hackathon_results = hackathon['results']
+    for measure in hackathon_results:
+        if hackathon_results[measure] != None:
+            if type(hackathon_results[measure]) is dict:
+                for sub_measure in hackathon_results[measure]:
+                    values = hackathon_results[measure][sub_measure]
+                    if values != None:
+                        if measure not in statistics:
+                            statistics[measure] = {}
+                        statistics[measure][sub_measure] = create_statistics(hackathon_results[measure][sub_measure])
+            else:
+                statistics[measure] = create_statistics(hackathon_results[measure])
+    analysis = dict(hackathon)
+    analysis['results'] = statistics
+    return Analysis.model_validate(analysis)
+
 @router.get('')
 def get_analyses(
     hackathons_collection: Annotated[Collection, Depends(hackathons_collection)],
@@ -89,15 +135,16 @@ def get_analyses(
     '''Create all analyses given a list of filters'''
     hackathon_ids_list = hackathons.split(',')
     hackathon = build_single_hackathon(hackathon_ids_list, hackathons_collection)
-    encoded_filters = json.loads(filters)
-    filtered_hackathons = []
-    if len(encoded_filters) == 0:
-        encoded_filters.append({})
-    for filter_combination in encoded_filters:
+    statistics = create_analysis(hackathon)
+    decoded_filters = json.loads(filters)
+    filtered_statistics = []
+    if len(decoded_filters) == 0:
+        decoded_filters.append({})
+    for filter_combination in decoded_filters:
         filtered_hackathon = build_filtered_hackathon(filter_combination, hackathons_collection)
         if filtered_hackathon != None:
-            filtered_hackathons.append(filtered_hackathon)
+            filtered_statistics.append(create_analysis(filtered_hackathon))
     return {
-        'selected': Hackathon.model_validate(hackathon),
-        'comparisons': [Hackathon.model_validate(filtered) for filtered in filtered_hackathons]
+        'selected': statistics,
+        'comparisons': filtered_statistics
     }
