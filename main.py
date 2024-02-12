fc486cd510c0787dc10c0e6e2d3fb91f6115f168
sobertrauter <robert_sauter@gmx.de>
2024-02-12 12:49:42 +0100

    add fe url

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173'],
+    allow_origins=['http://localhost:5173', 'https://hack-eval.vercel.app'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
