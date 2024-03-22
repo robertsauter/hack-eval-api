f5562bd93f90e32decc228bf01e5d7f0b1a303da
sobertrauter <robert_sauter@gmx.de>
2024-03-22 13:26:54 +0100

    add download csv data functionality

--- db_setup.py
+++ db_setup.py
@@ -6,10 +6,12 @@ import routers.hackathons as hack
 from models.Hackathon import Hackathon
 from data.survey_questions import QUESTIONS
 import copy
+from datetime import datetime
 
 CONNECTION_STRING = 'mongodb://localhost:27017'
 DB_NAME = 'hack-eval'
 FOLDER_PATH = ''
+USER_ID = ''
 
 client = MongoClient(CONNECTION_STRING)
 print('Connected to MongoDB')
@@ -17,18 +19,32 @@ print('Connected to MongoDB')
 database = client[DB_NAME]
 hackathons = database['hackathons']
 
-overview = pd.read_excel(f'{FOLDER_PATH}/!hackathons-overview.xlsx')
+overview = pd.read_excel(f'{FOLDER_PATH}/hackathons-overview.xlsx')
 
 for i, found_hackathon in overview.iterrows():
     hackathon_id = found_hackathon['ID']
+    start_day, start_month, start_year = found_hackathon['start'].split('.')
+    start = datetime(
+        year=int(start_year),
+        month=int(start_month),
+        day=int(start_day)
+    )
+    end_day, end_month, end_year = found_hackathon['end'].split('.')
+    end = datetime(
+        year=int(end_year),
+        month=int(end_month),
+        day=int(end_day)
+    )
     hackathon = Hackathon(
         title=hackathon_id,
         incentives=found_hackathon['incentives'],
         size=found_hackathon['size'],
         types=found_hackathon['type'].split(','),
         venue=found_hackathon['venue'],
+        start=start,
+        end=end,
         results=copy.deepcopy(QUESTIONS),
-        created_by=''
+        created_by=USER_ID
     )
     file = pd.read_csv(f'{FOLDER_PATH}/{hackathon_id}.csv')
     hack.map_hackathon_results_csv(hackathon, file)
--- routers/hackathons.py
+++ routers/hackathons.py
@@ -155,6 +155,43 @@ def map_hackathon_results_google(empty_hackathon: Hackathon, raw_hackathon: RawH
                             question, response.answers, questions_in_hackathon[question.title])
 
 
+def concatenate_list_of_dict(dict_of_lists: dict[str, list], key: str, values: list) -> None:
+    '''Concatenate a list in a dict with another list'''
+    if key in dict_of_lists:
+        dict_of_lists[key] += values
+    else:
+        dict_of_lists[key] = values
+
+
+def get_numerical_values(question: SurveyMeasure, values: list[str | int]) -> list[int]:
+    '''Get the values of a question or subquestion in numerical form'''
+    numerical_values = []
+    for value in values:
+        if question.answer_type == 'string':
+            numerical_values.append(question.answers.index(value))
+        else:
+            numerical_values.append(value)
+    return numerical_values
+
+
+def fill_dict_with_hackathon_information(dict_of_lists: dict[str, list], hackathon: Hackathon, participants: int) -> None:
+    '''Fill a dict with all hackathon information values for all participants'''
+    concatenate_list_of_dict(dict_of_lists, 'id', [
+                             hackathon.title for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'start', [
+                             hackathon.start.strftime('%d.%m.%y') for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'end', [
+                             hackathon.end.strftime('%d.%m.%y') for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'incentives', [
+                             hackathon.incentives for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'venue', [
+                             hackathon.venue for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'size', [
+                             hackathon.size for i in range(participants)])
+    concatenate_list_of_dict(dict_of_lists, 'types', [
+                             ', '.join(hackathon.types) for i in range(participants)])
+
+
 @router.post('/csv')
 def upload_hackathon_csv(
     title: Annotated[str, Form()],
@@ -239,6 +276,60 @@ def get_hackathons_by_user_id(
             link=hackathon['link']
         ))
     return found_hackathons
+
+
+@router.get('/aggregated/csv')
+def get_aggregated_hackathon_from_user_id(
+    hackathons: Annotated[Collection, Depends(hackathons_collection)],
+    token: Annotated[str, Depends(OAUTH2_SCHEME)]
+) -> str:
+    '''Return a csv string, that contains all data of the uploaded hackathons of the logged in user'''
+    user_id = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])['sub']
+    result: dict[str, list] = {}
+    for raw_hackathon in hackathons.find({'created_by': user_id}):
+        hackathon = Hackathon.model_validate(raw_hackathon)
+
+        # Get number of participants, by getting the first question with a filled list of values
+        participants = None
+        for question in hackathon.results:
+            if question.values != None and len(question.values) > 0:
+                participants = len(question.values)
+                break
+        fill_dict_with_hackathon_information(result, hackathon, participants)
+
+        if participants != None:
+            for question in hackathon.results:
+                # Handle questions with subquestions
+                if question.question_type == 'group_question' or question.question_type == 'score_question':
+                    for sub_question in question.sub_questions:
+                        title = f'{question.title} [{sub_question.title}]'
+                        if len(sub_question.values) == 0:
+                            concatenate_list_of_dict(
+                                result, title, [None for i in range(participants)])
+                        else:
+                            numerical_values = get_numerical_values(
+                                question, sub_question.values)
+                            concatenate_list_of_dict(
+                                result, title, numerical_values)
+                # Handle questions without subquestions
+                else:
+                    if len(question.values) == 0:
+                        concatenate_list_of_dict(
+                            result, question.title, [None for i in range(participants)])
+                    else:
+                        numerical_values = numerical_values = get_numerical_values(
+                            question, question.values)
+                        concatenate_list_of_dict(
+                            result, question.title, numerical_values)
+
+        # Raise an exception, if no answers were found in the hackathon
+        # Note: The above check for participants ignores questions with subquestions, so this might not be accurate in the future
+        # (but right now every hackathon should have values for at least one question without subquestions, so it should be accurate)
+        else:
+            raise Exception(
+                f'Hackathon {hackathon.title} seems to have no answers!')
+    values_df = pd.DataFrame(result)
+    return values_df.to_csv(index=False)
 
 
 @router.delete('/{hackathon_id}')
