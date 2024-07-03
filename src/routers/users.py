02ae72878c8a0dec4920ff27943a277f3d90d061
sobertrauter <robert_sauter@gmx.de>
2024-07-03 19:11:37 +0200

    add user edit routes

--- src/routers/users.py
+++ src/routers/users.py
@@ -1,6 +1,6 @@
 '''Routes for handling user authentication'''
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from src.models.User import UserInDB
 from src.models.Token import Token
 from typing import Annotated
@@ -11,7 +11,8 @@ from src.lib.database import users_collection
 from src.lib.http_exceptions import HTTP_401, HTTP_409
 from datetime import timedelta, datetime
 from jose import jwt
-from src.lib.globals import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from src.lib.globals import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, OAUTH2_SCHEME
+from bson.objectid import ObjectId
 
 password_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -73,3 +74,50 @@ def login(
         HTTP_401('Username or password not found')
     access_token: str = create_access_token(user.id)
     return Token(access_token=access_token, token_type='bearer')
+
+
+@router.post('/password')
+def update_password(
+    old_password: Annotated[str, Form()],
+    new_password: Annotated[str, Form()],
+    token: Annotated[str, Depends(OAUTH2_SCHEME)],
+    users: Annotated[Collection, Depends(users_collection)]
+):
+    '''Update the password of a user'''
+    user_id = ObjectId(jwt.decode(token, SECRET_KEY,
+                       algorithms=[ALGORITHM])['sub'])
+    user = users.find_one({'_id': user_id})
+    password_correct = password_context.verify(
+        old_password, user['hashed_password'])
+    if user is None or not password_correct:
+        HTTP_401('Password incorrect')
+    users.update_one(
+        {
+            '_id': user_id
+        },
+        {
+            '$set': {'hashed_password': password_context.hash(new_password)}
+        }
+    )
+    return 'Success'
+
+
+@router.post('/username')
+def update_username(
+    username: Annotated[str, Form()],
+    token: Annotated[str, Depends(OAUTH2_SCHEME)],
+    users: Annotated[Collection, Depends(users_collection)]
+):
+    '''Update the username of a user'''
+    user_id = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])['sub']
+    if users.find_one({'username': username}) is not None:
+        HTTP_409('Username already taken')
+    users.update_one(
+        {
+            '_id': ObjectId(user_id)
+        },
+        {
+            '$set': {'username': username}
+        }
+    )
+    return 'Success'
